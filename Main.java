0cfec180f601df2fcba55e44371b7b0a609def81
daBoss <serge.loie@yandex.ru>
2024-05-19 11:54:19 +0500

    add 125. Valid Palindrome solution

--- Main.java
+++ Main.java
@@ -1,16 +1,23 @@
-import TwoPointers.ValidPalindrome.Solution;
+import TwoPointers.ValidPalindrome.Solution2;
 
 import java.util.List;
 
 
 public class Main {
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        Solution2 solution = new Solution2();
         String test = "A man, a plan, a canal: Panama";
         String test2 = "race a car";
         String test3 = "0P";
-        boolean result = solution.isPalindrome(test3);
+        String test4 = ".,";
+        boolean result = solution.isPalindrome(test);
+        boolean result2 = solution.isPalindrome(test2);
+        boolean result3 = solution.isPalindrome(test3);
+        boolean result4 = solution.isPalindrome(test4);
         System.out.println(result);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
 
     }
 }
