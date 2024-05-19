c5e043d79c0ef3c4b1a38117013a75b08852f115
daBoss <serge.loie@yandex.ru>
2024-05-19 22:09:00 +0500

    add 167. Two Sum II - Input Array Is Sorted solution

--- Main.java
+++ Main.java
@@ -1,23 +1,18 @@
-import TwoPointers.ValidPalindrome.Solution2;
+import TwoPointers.TwoSum2InputArrayIsSorted.Solution;
 
+import java.util.Arrays;
 import java.util.List;
 
 
 public class Main {
     public static void main(String[] args) {
-        Solution2 solution = new Solution2();
-        String test = "A man, a plan, a canal: Panama";
-        String test2 = "race a car";
-        String test3 = "0P";
-        String test4 = ".,";
-        boolean result = solution.isPalindrome(test);
-        boolean result2 = solution.isPalindrome(test2);
-        boolean result3 = solution.isPalindrome(test3);
-        boolean result4 = solution.isPalindrome(test4);
-        System.out.println(result);
-        System.out.println(result2);
-        System.out.println(result3);
-        System.out.println(result4);
+        Solution solution = new Solution();
+        int[] test1 = {2,7,11,15};
+        int[] test2 = {2,3,4};
+        int[] result = solution.twoSum(test1, 9);
+        int[] result2 = solution.twoSum(test2, 6);
+        System.out.println(Arrays.toString(result));
+        System.out.println(Arrays.toString(result2));
 
     }
 }
